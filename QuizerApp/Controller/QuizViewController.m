197034c55ec2b0317953e7f48f3edadefa1189e2
Balachander M <balachander.m@freshdesk.com>
2015-05-12 11:40:53 +0530

    Added random colors

--- QuizerApp/Controller/QuizViewController.m
+++ QuizerApp/Controller/QuizViewController.m
@@ -17,6 +17,8 @@
     NSInteger currentQuestionIndex;
     Question *currentQuestion;
     NSArray *colors;
+    NSMutableArray *randomColors;
+    BOOL isNextQuestion;
 }
 @property (nonatomic, retain) IBOutlet UILabel *questionLabel;
 @property (nonatomic, retain) IBOutlet UITableView *optionsListTableView;
@@ -28,18 +30,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
  
-    currentQuestionIndex = 0;
-    if ([NetworkManager questions].count) {
-        
-        currentQuestion = [NetworkManager questions][currentQuestionIndex];
-        [self changeContentToCurrentQuestion];
+    currentQuestionIndex = -1;
+    
+    colors = @[[UIColor greenColor], [UIColor redColor], [UIColor orangeColor], [UIColor yellowColor]];
+    randomColors = [NSMutableArray new];
+    
+    [self changeQuestionAndReloadView];
+}
+
+//TODO: Move the Presentation logics to Cell/ViewModel...
+
+- (NSInteger)getRandomNonRepeatingNumberWithinFour {
+
+    if (randomColors.count) {
+        NSInteger randomIndex = arc4random() % [randomColors count];
+        return randomIndex;
     }
+    return 0;
+}
+
+- (UIColor *)getColorForRandomIndex:(NSInteger)randomIndex {
+ 
+    if (randomIndex < randomColors.count) {
+        UIColor *randomColor = randomColors[randomIndex];
+        [randomColors removeObjectAtIndex:randomIndex];
+        return randomColor;
+    }
+    return nil;
 }
 
 - (void)changeQuestionAndReloadView {
 
     [self changeToNextQuestion];
-    [self changeContentToCurrentQuestion];
 }
 
 - (void)changeContentToCurrentQuestion {
@@ -51,7 +73,22 @@
 - (void)changeToNextQuestion {
     
     currentQuestionIndex += 1;
-    currentQuestion = [NetworkManager questions][currentQuestionIndex];
+    if (currentQuestionIndex < [NetworkManager questions].count) {
+        
+        currentQuestion = [NetworkManager questions][currentQuestionIndex];
+
+        // Reloading RandomColors Array from first
+        [randomColors addObjectsFromArray:colors];
+        
+        // And Reload the content
+        [self changeContentToCurrentQuestion];
+
+    } else {
+        // Alert the user that question is over
+        [[[UIAlertView alloc] initWithTitle:@"Alert!!!" message:@"Thalaiva, you are great! /n GAME OVER" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
+        
+        [self.navigationController popViewControllerAnimated:YES];
+    }
 }
 
 #pragma mark - UITableView Datasource
@@ -64,10 +101,21 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     OptionsCell *cell = [tableView dequeueReusableCellWithIdentifier:@"OptionCell"];
-    
+
     cell.choiceLabel.text = [NSString stringWithFormat:@"%@", currentQuestion.choices[indexPath.row]];
+    UIColor *backgroundColor = [self getColorForRandomIndex:[self getRandomNonRepeatingNumberWithinFour]];
+    if (backgroundColor) {
+        cell.backgroundColor = backgroundColor;
+    } else {
+        cell.backgroundColor = [UIColor blackColor];
+    }
     
     return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    return tableView.frame.size.height / 4;
 }
 
 #pragma mark - UITableView Delegates
