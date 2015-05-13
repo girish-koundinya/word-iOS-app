1d4fb8fe8dc4471d25ff0483100a31c846665bb2
Balachander M <balachander.m@freshdesk.com>
2015-05-13 18:56:41 +0530

    Added Animation

--- QuizerApp/Controller/QuizViewController.m
+++ QuizerApp/Controller/QuizViewController.m
@@ -11,6 +11,39 @@
 #import "NetworkManager.h"
 #import "Question.h"
 
+@interface UIColor (AppColor)
+
++ (UIColor *)appOrangeColor;
++ (UIColor *)appBlueColor;
++ (UIColor *)appGreenColor;
++ (UIColor *)appRedColor;
+
+@end
+
+@implementation UIColor (AppColor)
+
++ (UIColor *)appOrangeColor {
+    
+    return [UIColor colorWithRed:231.0/255.0 green:149.0/255.0 blue:77.0/255.0 alpha:1.0];
+}
+
++ (UIColor *)appBlueColor {
+    
+    return [UIColor colorWithRed:60.0/255.0 green:122.0/255.0 blue:218.0/255.0 alpha:1.0];
+}
+
++ (UIColor *)appGreenColor {
+    
+    return [UIColor colorWithRed:45.0/255.0 green:180.0/255.0 blue:115.0/255.0 alpha:1.0];
+}
+
++ (UIColor *)appRedColor {
+    
+    return [UIColor colorWithRed:179.0/255.0 green:57.0/255.0 blue:61.0/255.0 alpha:1.0];
+}
+
+@end
+
 @interface QuizViewController () <UITableViewDataSource, UITabBarDelegate>
 {
     NSArray *options;
@@ -18,10 +51,11 @@
     Question *currentQuestion;
     NSArray *colors;
     NSMutableArray *randomColors;
-    BOOL isNextQuestion;
 }
+
 @property (nonatomic, retain) IBOutlet UILabel *questionLabel;
 @property (nonatomic, retain) IBOutlet UITableView *optionsListTableView;
+@property (nonatomic, retain) IBOutlet UIImageView *resultImageView;
 
 @end
 
@@ -29,28 +63,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
- 
+    
     currentQuestionIndex = -1;
     
-    colors = @[[UIColor greenColor], [UIColor redColor], [UIColor orangeColor], [UIColor yellowColor]];
+    colors = @[[UIColor appGreenColor], [UIColor appRedColor], [UIColor appOrangeColor], [UIColor appBlueColor]];
     randomColors = [NSMutableArray new];
-    
+    _optionsListTableView.bounces = NO;
     [self changeQuestionAndReloadView];
 }
 
 //TODO: Move the Presentation logics to Cell/ViewModel...
 
 - (NSInteger)getRandomNonRepeatingNumberWithinFour {
-
+    
     if (randomColors.count) {
         NSInteger randomIndex = arc4random() % [randomColors count];
         return randomIndex;
     }
-    return 0;
+    return -1;
 }
 
 - (UIColor *)getColorForRandomIndex:(NSInteger)randomIndex {
- 
+    
     if (randomIndex < randomColors.count) {
         UIColor *randomColor = randomColors[randomIndex];
         [randomColors removeObjectAtIndex:randomIndex];
@@ -60,7 +94,7 @@
 }
 
 - (void)changeQuestionAndReloadView {
-
+    
     [self changeToNextQuestion];
 }
 
@@ -76,13 +110,13 @@
     if (currentQuestionIndex < [NetworkManager questions].count) {
         
         currentQuestion = [NetworkManager questions][currentQuestionIndex];
-
+        
         // Reloading RandomColors Array from first
         [randomColors addObjectsFromArray:colors];
         
         // And Reload the content
         [self changeContentToCurrentQuestion];
-
+        
     } else {
         // Alert the user that question is over
         [[[UIAlertView alloc] initWithTitle:@"Alert!!!" message:@"Thalaiva, you are great! /n GAME OVER" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
@@ -101,7 +135,7 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     OptionsCell *cell = [tableView dequeueReusableCellWithIdentifier:@"OptionCell"];
-
+    
     cell.choiceLabel.text = [NSString stringWithFormat:@"%@", currentQuestion.choices[indexPath.row]];
     UIColor *backgroundColor = [self getColorForRandomIndex:[self getRandomNonRepeatingNumberWithinFour]];
     if (backgroundColor) {
@@ -123,16 +157,34 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
     if ([currentQuestion.choices[indexPath.row] isEqualToString:currentQuestion.answer]) {
-        [[[UIAlertView alloc] initWithTitle:@"Alert!!!" message:@"Correct" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
+        
+        _resultImageView.image = [UIImage imageNamed:@"correct.png"];
     } else {
-        [[[UIAlertView alloc] initWithTitle:@"Alert!!!" message:@"Oops! Wrong" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
+        
+        _resultImageView.image = [UIImage imageNamed:@"wrong.png"];
     }
+    _resultImageView.backgroundColor = [self.optionsListTableView cellForRowAtIndexPath:indexPath].backgroundColor;
+    
+    [self showResultWithAnimation];
 }
 
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-
-    [self changeQuestionAndReloadView];
+- (void)showResultWithAnimation {
+    
+    [UIView animateWithDuration:0.40 animations:^{
+        
+        _resultImageView.alpha = 1.0;
+        _optionsListTableView.userInteractionEnabled = NO;
+    } completion:^(BOOL finished) {
+        
+        [UIView animateWithDuration:.4 animations:^{
+            
+            _resultImageView.alpha = 0.0;
+        } completion:^(BOOL finished) {
+            
+            _optionsListTableView.userInteractionEnabled = YES;
+            [self changeQuestionAndReloadView];
+        }] ;
+    }];
 }
-
 
 @end
